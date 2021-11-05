186e3a9ec224cb286ef54bdd4bc95025929a9f36
Nicolas luongo <nicolasluongo9@gmail.com>
2021-11-05 20:17:35 -0300

    Cambio de clases,metodos y creacion de nuevas
    *Creacion Carpeta permits y archivo de los permisos de cada usuario segun su rol
    *creacion de un user general, que luego extenderan los usuarios y empresa
    *eliminacion de Person
    *Ahora student extiende de User ya que se elimino person
    Modificaciones en los add del dao de student y controller de student

--- Models/Empresa.php
+++ Models/Empresa.php
@@ -2,14 +2,16 @@
 
 namespace Models;
 
-class Empresa
+use User;
+
+class Empresa extends User
 {
 private $idEmpresa;
 private $name;
-private $countryOrigin;
-private $direction;
-private $description;
-private $img;
+// private $countryOrigin;
+// private $direction;
+// private $description;
+// private $img;
 
 
 function __contructor(){
@@ -25,26 +27,30 @@ public function getName(){ return $this->name; }
 public function setName($name) { $this->name = $name; return $this;}
 
 
+public function getIdEmpresa(){ return $this->idEmpresa; }
+public function setIdEmpresa($idEmpresa): self { $this->idEmpresa = $idEmpresa; return $this; }
  
-public function getCountryOrigin(){ return $this->countryOrigin; }
-public function setCountryOrigin($countryOrigin) { $this->countryOrigin = $countryOrigin; return $this; }
+// public function getCountryOrigin(){ return $this->countryOrigin; }
+// public function setCountryOrigin($countryOrigin) { $this->countryOrigin = $countryOrigin; return $this; }
 
 
-public function getDirection(){ return $this->direction; }
-public function setDirection($direction) { $this->direction = $direction; return $this; }
+// public function getDirection(){ return $this->direction; }
+// public function setDirection($direction) { $this->direction = $direction; return $this; }
 
 
-public function getDescription() { return $this->description; }
-public function setDescription($description) { $this->description = $description; return $this; }
+// public function getDescription() { return $this->description; }
+// public function setDescription($description) { $this->description = $description; return $this; }
 
 
-public function getIdEmpresa() { return $this->idEmpresa; }
-public function setIdEmpresa($idEmpresa) { $this->idEmpresa = $idEmpresa; return $this; }
+// public function getIdEmpresa() { return $this->idEmpresa; }
+// public function setIdEmpresa($idEmpresa) { $this->idEmpresa = $idEmpresa; return $this; }
 
 
 
-public function getImg(){ return $this->img; }
-public function setImg($img): self { $this->img = $img; return $this; }
+// public function getImg(){ return $this->img; }
+// public function setImg($img): self { $this->img = $img; return $this; }
+
+
 }
 
 
